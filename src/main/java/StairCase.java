b730eac337a85f5ad07e6933e00c379917f2cce9
unknown <jvictorsimao@hotmail.com>
2022-08-19 15:26:36 -0300

    Staircase

---
+++ src/main/java/StairCase.java
@@ -0,0 +1,32 @@
+import java.math.BigInteger;
+
+public class StairCase {
+    // This is a staircase of size : n = 4
+    //    #
+    //   ##
+    //  ###
+    // ####
+    // Its base and height are both equal to n.
+    // It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+    // Write a program that prints a staircase of size n
+    // The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n= 4.
+. 
+    public static void main(String args[]) {
+
+    }
+
+    public static void staircase(int n) {
+            for (int index = 1; index <= n; index++) {
+                for (int j = n; j > 0; j--) {
+                    if (j <= index) {
+                        System.out.print("#");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+            }
+    }
+
+}
